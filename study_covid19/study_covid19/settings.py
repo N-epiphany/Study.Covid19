b470e47e938f9890081c25bbd79f757437d926dd
Bhavya Srivastava <bs.epiphany14@gmail.com>
2023-06-29 23:44:33 +0530

    reboot

--- study_covid19/study_covid19/settings.py
+++ study_covid19/study_covid19/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+vtie2-ur=3(0(mzzou*ett0qo3zvfx2x&-4edufn+8(n2@7-#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
